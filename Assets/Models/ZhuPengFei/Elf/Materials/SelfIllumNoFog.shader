e9a3f3f2e7f0f7f9a6aacdad37699f8fa3bc425e
zhupengfei <543666461@qq.com>
2017-12-21 10:53:52 +0800

    [zhupengfei][宠物面板锚点-宠物显示-宠物属性-宠物移动和攻击的代码]

---
+++ Assets/Models/ZhuPengFei/Elf/Materials/SelfIllumNoFog.shader
@@ -0,0 +1,48 @@
+﻿// Upgrade NOTE: replaced 'mul(UNITY_MATRIX_MVP,*)' with 'UnityObjectToClipPos(*)'
+
+Shader "SDShader/SelfIllumNoFog_Object" {
+	Properties {
+		_MainTex ("Base (RGB)", 2D) = "white" {}
+		_Color ("Main Color", Color) = (1,1,1,1)
+	}
+	SubShader {
+		Tags { "Queue"="Geometry+55" "RenderType"="Opaque" }
+		LOD 200
+		
+		Fog { Mode Off }
+		pass{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			//#define SHADER_API_GLES
+			#include "UnityCG.cginc"
+
+			sampler2D _MainTex;
+			float4 _MainTex_ST;
+			float4 _Color;
+		
+			struct v2f{
+				float4 pos 		: SV_POSITION;
+				float2 uv 		: TEXCOORD0;
+				float4 retColor 	: TEXCOORD2;
+			};
+		
+			v2f vert(appdata_full v)
+			{
+				v2f o;
+				o.pos = UnityObjectToClipPos(v.vertex);
+				o.uv = TRANSFORM_TEX(v.texcoord,_MainTex);
+				o.retColor		=	(_Color*2.0f);
+				return o;
+			}
+		
+			half4 frag(v2f i) : COLOR
+			{
+				//float emis = 2-saturate(dot(i.flength,i.flength));//1.0f - saturate(i.flength/MajorLightColor.w);
+				return tex2D(_MainTex,i.uv)*i.retColor;//*emis;//tex2D(_MainTex,i.uv)*lm*2.0f*_Color*(1+emis*2.0f);
+			}
+			ENDCG
+		}
+	} 
+	FallBack "Diffuse"
+}
---
+++ Assets/Scripts/ZhuPengFei/PetScripts/PetMoveAndAttack.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PetMoveAndAttack : MonoBehaviour
+{
+
+    //英雄
+    GameObject hero;
+    //敌人
+    GameObject enemy;
+    //移动速度
+    public float speed = 1.0f;
+    //宠物动画组件
+    Animator petAnim;
+    //自动寻路组件
+    private NavMeshAgent petAgent;
+
+    //没遇见敌人
+    bool NoMeetEnemy = true;
+    //宠物是否攻击
+    bool IsPetAtk = true;
+
+
+    private void Start()
+    {
+        //获取英雄对象
+        hero = GameObject.FindGameObjectWithTag("Hero");
+        //获取敌人对象
+        enemy = GameObject.FindGameObjectWithTag("Enemy");
+        //获取动画组件
+        petAnim = this.GetComponent<Animator>();
+        //获取自动寻路组件
+        petAgent = this.GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        //如果找到英雄对象
+        if (hero != null)
+        {
+            //如果没遇到敌人
+            if (NoMeetEnemy)
+            {
+                //宠物与英雄距离超过一定距离后，宠物开始向英雄移动
+                if (Vector3.Distance(hero.transform.position, transform.position) > 5f)
+                {
+                    //调用宠物移动方法
+                    PetToMove();
+                    //播放宠物移动动画
+                    petAnim.SetBool("Run", true);
+                }
+                else
+                {
+                    //关闭宠物移动动画
+                    petAnim.SetBool("Run", false);
+                }
+                //宠物朝向英雄
+                transform.LookAt(hero.transform.position);
+            }
+            //如果宠物攻击
+            if (IsPetAtk)
+            {
+                //如果宠物与敌人距离小于1
+                if (Vector3.Distance(transform.position, enemy.transform.position) <= 1f)
+                {
+                    //宠物自动寻路到宠物本身位置
+                    petAgent.SetDestination(transform.position);
+                    //关闭攻击循环
+                    IsPetAtk = false;
+                    //播放攻击动画
+                    petAnim.SetBool("Atk", true);
+                }
+                //如果宠物与敌人距离在1~5之间
+                if (Vector3.Distance(transform.position, enemy.transform.position) >= 1f &&
+                    Vector3.Distance(transform.position, enemy.transform.position) <= 5f)
+                {
+                    //宠物自动寻路到敌人位置
+                    petAgent.SetDestination(enemy.transform.position);
+                    //关闭攻击循环
+                    IsPetAtk = false;
+                    //播放移动动画
+                    petAnim.SetBool("Run", true);
+                }
+                //如果宠物与敌人距离大于5
+                if (Vector3.Distance(transform.position, enemy.transform.position) > 5f)
+                {
+                    //调用宠物移动函数
+                    PetToMove();
+                    //播放移动动画
+                    petAnim.SetBool("Run", true);
+                }
+            }
+            //强制宠物跟随英雄
+            if (Input.GetKeyDown(KeyCode.M))
+            {
+                IsPetAtk = false;
+                NoMeetEnemy = true;
+            }
+            //强制宠物攻击敌人
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                IsPetAtk = true;
+                NoMeetEnemy = false;
+            }
+        }
+    }
+    //宠物移动到英雄位置的方法
+    void PetToMove()
+    {
+        transform.position = Vector3.Lerp(transform.position, hero.transform.position, Time.deltaTime * speed);
+    }
+}
+

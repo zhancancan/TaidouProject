4967cd5f9ff32f9000e2468d4a0627880b27bf5e
zhupengfei <543666461@qq.com>
2017-12-22 19:43:36 +0800

    [zhupengfei][宠物属性面板的重新整合--宠物属性面板的属性代码（组件获取）]

---
+++ Assets/Scripts/ZhuPengFei/PetScripts/PetInfo.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PetInfo : MonoBehaviour {
+
+    //宠物信息单例
+    public static PetInfo _petInstance;
+
+
+
+    //宠物属性
+    #region
+    /*
+     * 宠物名
+     * 头像
+     * 战斗力
+     * 体力数
+     */
+    #endregion
+    private string petName;           //宠物名
+    private string petHead;           //宠物头像
+    private float petHP;              //宠物生命值
+    private int starLevel;            //宠物星阶等级
+    private int petAtk;               //宠物攻击力
+    private int petDef;               //宠物防御力
+    private int petCombat;            //宠物战斗力
+    private string petSkill;          //宠物技能
+    private int petEnergy;            //宠物活力值
+
+
+
+    private float energyTimer;          //活力计时器
+    #region  get/set
+    public string PetName
+    {
+        get { return petName; }
+        set { petName = value; }
+    }
+    public string PetHead
+    {
+        get { return petHead; }
+        set { petHead = value; }
+    }
+    public float PetHP
+    {
+        get { return petHP; }
+        set { petHP = value; }
+    }
+    public int StarLevel
+    {
+        get { return starLevel; }
+        set { starLevel = value; }
+    }
+    public int PetAtk
+    {
+        get { return petAtk; }
+        set { petAtk = value; }
+    }
+    public int PetDef
+    {
+        get { return petDef; }
+        set { petDef = value; }
+    }
+    public int PetCombat
+    {
+        get { return petCombat; }
+        set { petCombat = value; }
+    }
+    public string PetSkill
+    {
+        get { return petSkill; }
+        set { petSkill = value; }
+    }
+    public int PetEnergy
+    {
+        get { return petEnergy; }
+        set { petEnergy = value; }
+    }
+    #endregion
+
+
+    void Awake()
+    {
+        _petInstance = this;
+    }
+
+
+    void Update()
+    {
+        //活力值的自动增长
+        if (this.energyTimer<100)
+        {
+            energyTimer += Time.deltaTime;
+            //60s增长一个活力值
+            if (this.energyTimer>=60)
+            {
+                petEnergy++;
+                energyTimer -= 60;
+            }
+        }
+        //活力值如果满了，就不增长，一直保持计时器为0
+        else
+        {
+            this.energyTimer = 0;
+        }
+    }
+
+    void petInit()
+    {
+        this.petName = "烈焰狂狮";
+        this.petHead = "Head_Lion";
+        this.petHP = 90;
+        this.starLevel = 1;
+        this.petAtk = 110;
+        this.PetDef = 95;
+        this.petCombat = 1125;
+        this.petSkill = "狮吼";
+        this.petEnergy = 95;
+    }
+
+
+}
---
+++ Assets/Scripts/ZhuPengFei/PetScripts/PetTopBG.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PetTopBG : MonoBehaviour
+{
+
+    private Slider hpSlider;          //血条
+    private Text petHPNum;            //血量
+    private Image star1;              //星1
+    private Image star2;              //星2
+    private Image star3;              //星3
+    private Image star4;              //星4
+    private Image star5;              //星5
+    private Text starLevelNum;        //星星等级
+    private RawImage headPic;        //头像
+    private Text petName;             //宠物名
+    private Text petAtkNum;           //宠物攻击力
+    private Text petDefNum;           //宠物护甲
+    private Text petCombatNum;        //宠物战斗力
+    private Text petSkillDescription; //宠物技能描述
+    private Text petEnergyNum;        //宠物活力
+    private Text petRecoverNum;       //宠物活力恢复时间
+    private Text petAllNum;           //宠物活力全部恢复时间
+
+    void Awake()
+    {
+        hpSlider = transform.Find("HPslider").GetComponent<Slider>();
+        star1= transform.Find("StarBG1/Star1").GetComponent<Image>();
+        star2= transform.Find("StarBG2/Star2").GetComponent<Image>();
+        star3= transform.Find("StarBG3/Star3").GetComponent<Image>();
+        star4= transform.Find("StarBG4/Star4").GetComponent<Image>();
+        star5 = transform.Find("StarBG5/Star5").GetComponent<Image>();
+        starLevelNum = transform.Find("StarLevelNum").GetComponent<Text>();
+        petHPNum = transform.Find("PetHPNum").GetComponent<Text>();
+        headPic = transform.Find("HeadCircle/Head").GetComponent<RawImage>();
+        petName = transform.Find("PetName").GetComponent<Text>(); 
+        petAtkNum = transform.Find("PetAtkNum").GetComponent<Text>();
+        petDefNum = transform.Find("PetDefNum").GetComponent<Text>();
+        petCombatNum = transform.Find("PetCombatNum").GetComponent<Text>();
+        petSkillDescription = transform.Find("PetSkillDescription").GetComponent<Text>();
+        petEnergyNum = transform.Find("PetEnergyNum").GetComponent<Text>();
+        petRecoverNum = transform.Find("PetRecoverNum").GetComponent<Text>();
+        petAllNum = transform.Find("PetAllNum").GetComponent<Text>();
+    }
+}

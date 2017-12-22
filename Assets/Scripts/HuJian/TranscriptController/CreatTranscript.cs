e01a4d86c352e3fbd310dab2ddf415fb0b45e54b
hujian <1123689907@qq.com>
2017-12-22 20:43:10 +0800

    [hujian][副本选择关卡生成场景]

---
+++ Assets/Scripts/HuJian/TranscriptController/CreatTranscript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class CreatTranscript : MonoBehaviour
+{
+
+    public void CreateTranscript()
+    {
+        SceneManager.LoadScene("Map_01");
+    }
+}

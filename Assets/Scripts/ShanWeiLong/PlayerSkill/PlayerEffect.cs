4da1a1ebfd3de0c1ff93cd1ed4afc119c920c359
zhangcancan <654894782@qq.com>
2018-01-03 21:50:51 +0800

    [shanweilong][人物动画]

---
+++ Assets/Scripts/ShanWeiLong/PlayerSkill/PlayerEffect.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerEffect : MonoBehaviour {
+
+    //攻击特效的渲染
+    Renderer[] rendererArray;
+    //这个脚本中带的重置特效的方法
+    NcCurveAnimation[] curveAnim;
+
+    GameObject effectOffset;
+
+    void Start () {
+        rendererArray = GetComponentsInChildren<Renderer>();
+        curveAnim = GetComponentsInChildren<NcCurveAnimation>();
+
+        if (transform.Find("EffectOffset") != null)
+        {
+            effectOffset = transform.Find("EffectOffset").gameObject;
+        }
+    }
+	
+    public void Show()
+    {
+        if (effectOffset != null)
+        {
+            effectOffset.SetActive(false);
+            effectOffset.SetActive(true);
+        }
+        else
+        {
+            //遍历打开渲染效果
+            foreach (Renderer renderer in rendererArray)
+            {
+                renderer.enabled = true;
+            }
+            //遍历重置特效
+            foreach (NcCurveAnimation anim in curveAnim)
+            {
+                anim.ResetAnimation();
+            }
+        }
+    }
+}

0fafbe3147df7fbdcd222cc28e6e77c9db974808
zhangcancan <654894782@qq.com>
2018-01-18 16:04:38 +0800

    [shanweilong][好友，战斗，锻造界面]

---
+++ Assets/Scripts/ShanWeiLong/UIForging/UIIForging.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIIForging : UIBase {
+
+    public override void OnEntering()
+    {
+        gameObject.SetActive(true);
+    }
+    public override void OnPausing()
+    {
+
+    }
+    public override void OnResuming()
+    {
+
+    }
+    public override void OnExiting()
+    {
+
+    }
+
+}

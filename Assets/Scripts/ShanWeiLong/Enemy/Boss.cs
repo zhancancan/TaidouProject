68ee70e8e2bba4f8c2284ab664061c894d5d5de1
zhangcancan <654894782@qq.com>
2018-01-20 10:25:13 +0800

    [shanweilong][怪物模型和脚本]

---
+++ Assets/Scripts/ShanWeiLong/Enemy/Boss.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss : MonoBehaviour
+{
+    private Transform player;
+    private Rigidbody rig;
+    private Animator anim;
+
+    private float angleView = 30;   //视野范围
+    private float attackDistance = 3;  //攻击距离
+    private float playerDistance = 15;
+    private float attackTime = 1;
+    private float timer = 0;
+    private float hp = 2000;
+    private bool isAttack = false;
+
+	void Start ()
+	{
+	    player = GameObject.FindGameObjectWithTag("Player").transform;
+	    rig = GetComponent<Rigidbody>();
+	    anim = GetComponent<Animator>();
+	}
+	
+	
+	void Update ()
+	{
+	    if (isAttack == true) return;
+	    Vector3 playerPos = player.position;
+        //将玩家的Y轴与Boss的Y轴同步
+        playerPos.y = transform.position.y;
+        //玩家与Boss前方的夹角
+	    float angle = Vector3.Angle(playerPos - transform.position, transform.forward);
+	    if (angle < angleView/2)
+	    {
+            //攻击视野之内
+            float distance = Vector3.Distance(player.position, transform.position);
+	        if (distance > playerDistance)
+	        {
+                anim.SetBool("Walk", false);
+                return;
+	        }
+	        else
+	        {
+                if (distance < attackDistance)
+                {
+                    //攻击
+                    if (isAttack == false)
+                    {
+                        timer += Time.deltaTime;
+                        if (timer >= attackTime)
+                        {
+                            Attack();
+                        }
+                    }
+                }
+                else
+                {
+                    //追击
+                    anim.SetBool("Walk", true);
+                    transform.position += transform.forward*2f*Time.deltaTime;
+                    //rig.MovePosition(transform.position + transform.forward * 2f * Time.deltaTime);
+                }
+	        }
+        }
+	    else
+	    {
+	        anim.SetBool("Walk", true);
+            //Boss朝向
+	        Quaternion targetRotation = Quaternion.LookRotation(playerPos - transform.position);
+            //旋转
+	        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, 1f*Time.deltaTime);
+	    }
+	}
+
+    private int index = 0;
+    void Attack()
+    {
+        anim.SetBool("Walk", false);
+        isAttack = true;
+        index++;
+        if (index == 4) index = 1;
+        anim.SetTrigger("Attack0" + index);
+    }
+
+    void BackToIdle()
+    {
+        isAttack = false;
+    }
+}
---
+++ Assets/Scripts/ShanWeiLong/Enemy/Enemy.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Enemy : MonoBehaviour
+{
+    private Transform player;
+    private CharacterController cc;
+    private int hp = 200;
+    private int hpTotal = 0;  //记录血量
+    private int attackRate = 2;  //攻击速率
+    private int damage = 20;  //攻击力
+    private float playerDistance = 15;   //人物跟怪物的距离
+    private float attackDistance = 2;
+    private float distance = 0;
+    private float attackTimer = 0; //计时
+
+    private Animation anim;
+
+    private GameObject bloodBar;
+    private GameObject bloodGo;
+    private Slider bloodSlider;
+
+    void Start ()
+	{
+        hpTotal = hp;
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        cc = GetComponent<CharacterController>();
+	    anim = GetComponent<Animation>();
+	    InvokeRepeating("CalcDistance", 0, 0.1f);
+        bloodBar = Resources.Load<GameObject>("BloodBg");
+	    Transform bloodPoint = transform.Find("BloodPoint");
+	    bloodSlider = bloodBar.transform.Find("Blood").GetComponent<Slider>();
+        bloodGo = Instantiate(bloodBar, bloodPoint.position, Quaternion.identity);
+	    bloodGo.transform.parent = bloodPoint;
+	}
+	
+	
+	void Update ()
+	{
+        if (hp <= 0)
+	    {
+	        transform.Translate(-transform.up*1f*Time.deltaTime);
+	        Destroy(gameObject, 3);
+	        return;
+	    }
+        distance = Vector3.Distance(player.transform.position, transform.position);
+        if (distance > playerDistance)
+	    {
+	        return;
+	    }
+	    else
+	    {
+	        if (distance < attackDistance)
+	        {
+	            attackTimer += Time.deltaTime;
+	            if (attackTimer > attackRate)
+	            {
+                    Vector3 targetPos = player.position;
+                    targetPos.y = transform.position.y;
+                    transform.LookAt(targetPos);
+                    //攻击
+                    anim.Play("attack01");
+	                attackTimer = 0;
+	            }
+                if (!anim.Play("attack01"))
+                {
+                    anim.CrossFade("idle");
+                }
+	        }
+	        else
+	        {
+	            anim.Play("walk");
+                Move();
+            }
+	    }
+	    bloodGo.transform.LookAt(Camera.main.transform.position);
+	}
+
+    void CalcDistance()
+    {
+        distance = Vector3.Distance(player.transform.position, transform.position);
+    }
+
+    void Move()
+    {
+        Vector3 targetPos = player.position;
+        targetPos.y = transform.position.y;
+        transform.LookAt(targetPos);
+        cc.SimpleMove(transform.forward*1f);
+    }
+
+    void TakeDamage()
+    {
+        if (hp <= 0) return;
+        //受到伤害
+        bloodSlider.value = (float) hp/hpTotal;
+        anim.Play("takedamage");
+        if (hp <= 0)
+        {
+            Dead();
+        }
+    }
+
+    void Attack()
+    {
+        distance = Vector3.Distance(player.transform.position, transform.position);
+        if (distance < attackDistance)
+        {
+            player.SendMessage("TakeDamage", damage, SendMessageOptions.DontRequireReceiver);
+        }
+    }
+
+    void Dead()
+    {
+        anim.Play("die");
+    }
+}

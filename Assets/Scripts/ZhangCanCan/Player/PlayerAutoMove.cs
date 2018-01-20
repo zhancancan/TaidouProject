701efe027540c3f9017395585aae61864fa5f3c7
hujian <1123689907@qq.com>
2018-01-20 17:48:50 +0800

    [hujian][自动寻路的优化]

--- Assets/Scripts/ZhangCanCan/Player/PlayerAutoMove.cs
+++ Assets/Scripts/ZhangCanCan/Player/PlayerAutoMove.cs
@@ -5,21 +5,33 @@ using UnityEngine.AI;
 
 public class PlayerAutoMove : MonoBehaviour
 {
-    private NavMeshAgent agent; 
+    private CharacterController playerCharacter;
+    private NavMeshAgent agent;
+    private PlayerDir playerDir; 
     private void Awake()
     {
-        agent = this.GetComponent<NavMeshAgent>();   
+        agent = this.GetComponent<NavMeshAgent>();
+        playerCharacter = this.GetComponent<CharacterController>();
+        playerDir = this.GetComponent<PlayerDir>();   
         agent.enabled = false;
     }
     private void Update()
     {
         if (agent.enabled)
         {
+            playerCharacter.enabled = false;
+            playerDir.enabled = false;
             if (agent.remainingDistance < 1.5&&agent.remainingDistance!=0)
             {                
                 agent.isStopped = true;           
                 agent.enabled = false;
                 TaskManager._instance.OnArriveDestination();
+            }
+            if (Input.GetMouseButtonDown(0))
+            {
+                agent.enabled = false;
+                playerCharacter.enabled = true;
+                playerDir.enabled = true;
             }
         }
       
--- Assets/Scripts/ZhangCanCan/Player/PlayerMovement.cs
+++ Assets/Scripts/ZhangCanCan/Player/PlayerMovement.cs
@@ -7,17 +7,9 @@ public class PlayerMovement : MonoBehaviour
 {
 
     public float speed = 0f;//人物的速度
-    Vector3 _moment; //移动方向
-    Rigidbody _rig;//人物身上的刚体
     Animator _aim;//人物身上的动画控制器
-    int _floorMask;//地板的层号
-    public float maxCamRayLength = 100f;//相机发射射线的最大长度
-
     private void Awake()
     {
-        //获取地板层号，为了检测射线碰撞
-        _floorMask = LayerMask.GetMask("Floor");
-        _rig = this.GetComponent<Rigidbody>();
         _aim = this.GetComponent<Animator>();
     }
     private void Update()
@@ -26,70 +18,20 @@ public class PlayerMovement : MonoBehaviour
         float v = Input.GetAxis("Vertical");
 
         //1.移动
-        Move(-h, -v);
-        //2.旋转
-        Turning();
+        Move();        
         //3.播放动画
         PlayAnimation(h,v);
     }
-    private void Move(float h, float v)
+    private void Move()
     {
-        _moment.Set(h, 0, v);
-        //这里的归一化是为了在电脑上处理向量的时候的统一性
-        //如果向量带有长度，那么CPU在计算的时候会根据自己的性能产生不一样的时间（配置高速度越快）
-        //归一化的目的就是保证无论什么配置在处理单位向量的时候，速度是一致的
-        _moment = _moment.normalized * speed * Time.deltaTime;
-        _rig.MovePosition(transform.position + _moment);
-    }
-    private void Turning()
-    {
-        Ray cameraRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-        //碰撞信息存储
-        RaycastHit floorHit;
-
-        //检测射线碰撞
-        if(Physics.Raycast(cameraRay,out floorHit, maxCamRayLength, _floorMask))
-        {
-            //获取人物的朝向（看向鼠标点的位置）
-            Vector3 playerToMousePosition = (floorHit.point - transform.position).normalized;
-            //固定Y轴
-            playerToMousePosition.y = 0f;
-            //获取新的旋转角度（四元数）
-            Quaternion newRotation = Quaternion.LookRotation(playerToMousePosition);
-            //实现人物旋转
-            _rig.MoveRotation(newRotation);
-        }
-    }
+        float v = Input.GetAxis("Vertical");
+        transform.position += transform.forward * v * Time.deltaTime * 3f;
+        float h = Input.GetAxis("Horizontal");
+        transform.Rotate(new Vector3(0, h, 0) * 2f);
+    }   
     private void PlayAnimation(float h, float v)
     {
         _aim.SetBool("Walk", h != 0 || v != 0);
-    }
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject == CreatEnemy._instance.points[0].gameObject)
-        {
-                        
-            CreatEnemy._instance.CreateFirstEnemy();
-        }
-        if (other.gameObject == CreatEnemy._instance.points[1].gameObject)
-        {
-                        
-            CreatEnemy._instance.CreateSecondEnemy();
-        }
-        if (other.gameObject == CreatEnemy._instance.points[2].gameObject)
-        {
-            
-            
-            CreatEnemy._instance.CreateThirdEnemy();
-        }
-        if (other.gameObject == CreatEnemy._instance.points[3].gameObject)
-        {
-            
-            CreatEnemy._instance.CreateFourthEnemy();
-        }
-    }
-
-
-
-
+        _aim.SetBool("Idle", h == 0 && v == 0);
+    }   
 }
